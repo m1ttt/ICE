98010a5102c363b3fdc3513d2771bef33e912a49
Mike <68821845+m1ttt@users.noreply.github.com>
2023-10-11 07:50:48 -0600

    Update luis.py
    Extendi el mensaje el mensaje

--- luis.py
+++ luis.py
@@ -1,7 +1,7 @@
 def dalebogueto(data):
     print("Dale Bogueto!")
     if data == "y como gritan los ñeros":
-        print("Au au au")
+        print("Au au au xdxd")
 
 if __name__ == "__main__":
     dalebogueto("y como gritan los ñeros")
