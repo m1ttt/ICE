e104f24c49d2dc496f8e543652b372bc1ad68c2f
m1ttt <68821845+m1ttt@users.noreply.github.com>
2023-10-11 07:38:28 -0600

    Editando

--- luis.py
+++ luis.py
@@ -1,1 +1,7 @@
-print("hola")
+def dalebogueto(data):
+    print("Dale Bogueto!")
+    if data == "y como gritan los ñeros":
+        print("Au au au")
+
+if __name__ == "__main__":
+    dalebogueto("y como gritan los ñeros")
